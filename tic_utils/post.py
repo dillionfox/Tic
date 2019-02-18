438f509506da41d35d1bd49a3fbaa5d3577c7435
dillionfox <11foxd1@gmail.com>
2019-02-18 14:46:51 -0500

    added special function for Stephen

--- tic_utils/post.py
+++ tic_utils/post.py
@@ -10,7 +10,7 @@ class PostProc:
 		self.corr = None
 		self.task = task
 		plotter.clearplt()
-		self.check_input()
+		#self.check_input()
 
 	def check_input(self):
 		if self.data.shape[0] != 2 and "correlate" in self.task:
@@ -24,6 +24,11 @@ class PostProc:
 			plotter.plot(self.corr,False,'Correlation')
 		return None
 
+	def corr_2(self):
+		if self.data.shape[0] == 1 and self.data.shape[1] == 2:
+			self.corr = autotools.corr(self.data[0][0].dG, self.data[0][1].dG)
+			plotter.plot(self.corr,False,'Correlation')
+
 	def stats(self):
 		for i in range(len(self.data)):
 			print(i, self.data[i].shape)
@@ -32,6 +37,8 @@ class PostProc:
 		if "correlate" in self.task:
 			self.run_corr()
 			self.stats()
+		if "corr_2" in self.task:
+			self.corr_2()
 		return None
 
 	def plot(self):
